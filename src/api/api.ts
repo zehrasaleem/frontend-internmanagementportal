52854c8b3fa2fe11d2bbb96e2e2a905899e37539
Zehra Saleem <zehrasaleem022@gmail.com>
2026-05-03 14:30:14 +0500

    Prepare project for deployment

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,7 +6,8 @@ import axios, {
   AxiosHeaders,
   AxiosRequestConfig,
 } from "axios";
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL =
+  import.meta.env.VITE_API_URL ?? "http://localhost:5000/api";
 
 /* ------------------------------------------------------------
    ✅ Create base Axios instance
